f25c4c8b29b768d653723cebfbff5b94f75a1499
Monoblaster <64797269+Monoblaster@users.noreply.github.com>
2021-03-31 14:14:18 -0700

    Update Version and Changelog

--- Event_Variables/server.cs
+++ Event_Variables/server.cs
@@ -4,10 +4,10 @@
 //Helpers: Clockturn, Truce, Boom, Lilboarder32, Chrono, and Monoblaster
 //------
 //Major: 7
-//Minor: 2
-//Patch: 0
-//Total: 7.20
-$VCE::Server::Version = "7.42";
+//Minor: 4
+//Patch: 3
+//Total: 7.43
+$VCE::Server::Version = "7.43";
 //---
 // Support
 //---
